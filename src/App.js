deff2cf43340c98f64d9c130ca69e81d35d066a2
lljmb <brown.jessm@icloud.com>
2020-11-04 15:53:50 -0500

    photo of the day one

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [photo, setPhoto] = useState(null)
+
+useEffect(() => { 
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then( (res) => {
+    console.log('hey hey hey')
+  })
+  .catch( err => {
+    debugger;
+  })
+})  
+
+
+
   return (
     <div className="App">
       <p>
