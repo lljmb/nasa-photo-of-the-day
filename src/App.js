9b82fdf89ba3f5bcc193680c23758afc927f5d8a
lljmb <brown.jessm@icloud.com>
2020-11-05 22:30:32 -0500

    photo loads

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import styled from 'styled-components'
+import Header from './styles/Header.less'
+import Body from './styles/Body.less'
 
 function App() {
-  const [photo, setPhoto] = useState(null)
+  const [photo, setPhoto] = useState([])
 
-useEffect(() => { 
-  axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-  .then( (res) => {
-    console.log('hey hey hey')
-  })
-  .catch( err => {
-    debugger;
-  })
-})  
-
-
+  useEffect(() => { 
+    const fetchPhoto = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-11-05')
+    .then( (res) => {
+      setPhoto(res.data.url);
+      console.log('hey hey hey');
+    })
+    .catch( err => {
+      debugger;
+    })
+  }
+    fetchPhoto();
+  }, [])  
 
   return (
     <div className="App">
+      <h1>NASA Photo of the Day</h1>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <img src={photo}></img>
+       <button><span role="img" aria-label='go!'>🚀!</span></button>
       </p>
     </div>
   );
 }
+
+const StyledNASA = styled.div`
+opacity: 0;
+
+width: 60%;
+display: flex;
+justify-content: space-between;
+
+button {
+  color: ${(props) => props.theme.tertiaryColor};
+  &:hover{
+    transform: scale(1.3);
+    transition: transform 0.5s ease-in-out;
+  }
+  transition: transform 0.5s ease-in-out;
+}
+
+@media(max-width: 550px) {
+  width: 100%auto;
+}
+
+&:hover {
+  color: ${(props) => props.theme.secondaryColor};
+  background-color: lightcoral;
+  transition: all 0.2s ease-in-out;
+}
+transition: all 0.2s ease-in-out;
+`
 
 export default App;
